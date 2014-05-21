415757241fd5f0f8f45e6c30d2377dbf2df8f18c
Rémi Weng <ixartz@users.noreply.github.com>
2014-05-21 15:28:03 +0200

    Implement a basic version of Viola-Jones method

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,31 @@
 int main()
 {
     cv::VideoCapture capture(0);
-    cv::Mat cameraFrame;
+    cv::Mat camera_frame;
+    cv::Mat camera_frame_gray;
+    std::vector<cv::Rect> faces;
     char key = 0;
+
+    cv::CascadeClassifier face_cascade("../haarcascade_frontalface_default.xml");
+    cv::CascadeClassifier eye_cascade("../haarcascade_eye.xml");
 
     cv::namedWindow("Face detection", cv::WINDOW_AUTOSIZE);
 
     while (key != 'q')
     {
-        capture.read(cameraFrame);
-        cv::flip(cameraFrame, cameraFrame, 1);
-        cv::imshow("Face detection", cameraFrame);
+        capture.read(camera_frame);
+        cv::flip(camera_frame, camera_frame, 1);
+        cvtColor(camera_frame, camera_frame_gray, CV_BGR2GRAY);
+
+        face_cascade.detectMultiScale(camera_frame_gray, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, cv::Size(30, 30));
+
+        for (size_t i = 0; i < faces.size(); ++i)
+        {
+            cv::Point center(faces[i].x + faces[i].width * 0.5, faces[i].y + faces[i].height * 0.5);
+            cv::ellipse(camera_frame, center, cv::Size(faces[i].width * 0.5, faces[i].height * 0.5), 0, 0, 360, cv::Scalar(255, 0, 255), 4, 8, 0);
+        }
+
+        cv::imshow("Face detection", camera_frame);
 
         key = cvWaitKey(1000 / 30);
     }
