e1291b03996b197ef71b97cb944a85980dfdd007
Rémi Weng <ixartz@users.noreply.github.com>
2014-05-22 20:03:13 +0200

    Add face haar XML files

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,8 @@ int main()
     std::vector<cv::Rect> eyes;
     cv::Scalar color_face(255, 0, 255);
     cv::Scalar color_eye( 255, 0, 0 );
+    cv::Mat faceROI;
+    cv::Mat eyeROI;
     char key = 0;
 
     cv::CascadeClassifier face_cascade("../haarcascade_frontalface_alt.xml");
@@ -32,7 +34,7 @@ int main()
             cv::Point center(faces[i].x + faces[i].width * 0.5, faces[i].y + faces[i].height * 0.5);
             cv::ellipse(camera_frame, center, cv::Size(faces[i].width * 0.5, faces[i].height * 0.5), 0, 0, 360, color_face, 4, 8, 0);
 
-            cv::Mat faceROI = camera_frame_gray(faces[i]);
+            faceROI = camera_frame_gray(faces[i]);
             eyes.clear();
 
             eye_cascade.detectMultiScale(faceROI, eyes, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, cv::Size(30, 30));
@@ -42,6 +44,8 @@ int main()
                 cv::Point center(faces[i].x + eyes[j].x + eyes[j].width * 0.5, faces[i].y + eyes[j].y + eyes[j].height * 0.5);
                 int radius = cvRound((eyes[j].width + eyes[j].height) * 0.25);
                 cv::circle(camera_frame, center, radius, color_eye, 4, 8, 0);
+
+                eyeROI = camera_frame_gray(eyes[0]);
             }
         }
 
